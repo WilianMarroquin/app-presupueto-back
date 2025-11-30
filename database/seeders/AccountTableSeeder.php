2cd8a87492bbf858b631d4c6d2f9352962bf593b
WilianMarroquin <githubwiliangit@gmail.com>
2025-11-30 14:44:58 -0600

    Improvements Seeders.

--- database/seeders/AccountTableSeeder.php
+++ database/seeders/AccountTableSeeder.php
@@ -22,40 +22,51 @@ class AccountTableSeeder extends Seeder
         CreditCardDetail::truncate();
 
         Account::create([
-            'name' => 'Cooperativa Guayacán',
+            'name' => 'Guayacán',
             'type_id' => AccountType::BANK,
             'currency_id' => AccountCurrency::QUETZAL,
-            'initial_balance' => 0,
-            'current_balance' => 1000,
+            'initial_balance' => 2188.83,
+            'current_balance' => 2188.83,
+            'nature' => 'asset',
+            'is_active' => 1,
+            'description' => 'Cuenta Transaccional',
+        ]);
+
+        Account::create([
+            'name' => 'Guayacán',
+            'type_id' => AccountType::BANK,
+            'currency_id' => AccountCurrency::QUETZAL,
+            'initial_balance' => 31.89,
+            'current_balance' => 31.89,
             'nature' => 'asset',
             'is_active' => 1,
             'description' => 'Cuenta de Ahorros en Cooperativa Guayacán',
         ]);
 
         Account::create([
-            'name' => 'Cooperativa Guayacán (Ahorro)',
+            'name' => 'BAC Credomatic',
             'type_id' => AccountType::BANK,
             'currency_id' => AccountCurrency::QUETZAL,
-            'initial_balance' => 0,
-            'current_balance' => 0,
+            'initial_balance' => 32.28,
+            'current_balance' => 32.28,
             'nature' => 'asset',
             'is_active' => 1,
-            'description' => 'Cuenta Transaccional',
+            'description' => 'Cuenta de Ahorros en Cooperativa Guayacán',
         ]);
 
         $tcBack = Account::create([
             'name' => 'TC BAC Credomatic',
             'type_id' => AccountType::CREDIT_CARD,
             'currency_id' => AccountCurrency::QUETZAL,
-            'initial_balance' => 0,
-            'current_balance' => 0,
+            'initial_balance' => 2204.87,
+            'current_balance' => 2204.87,
             'nature' => 'liability',
             'is_active' => 1,
             'description' => 'Tarjeta de Crédito BAC Credomatic',
         ]);
 
         $tcBack->creditCardDetail()->create([
-            'alias' => 'Bac Gasolineras',
+            'alias' => 'Cash Back Gasolineras',
             'network' => 'Mastercard',
             'color' => '#213796',
             'last_4' => 8428,
@@ -65,18 +76,18 @@ class AccountTableSeeder extends Seeder
         ]);
 
         $tcCope = Account::create([
-            'name' => 'TC Cooperativa Guayacán',
+            'name' => 'TC Guayacán',
             'type_id' => AccountType::CREDIT_CARD,
             'currency_id' => AccountCurrency::QUETZAL,
-            'initial_balance' => 0,
-            'current_balance' => 0,
+            'initial_balance' => 3536.49,
+            'current_balance' => 3536.49,
             'nature' => 'liability',
             'is_active' => 1,
             'description' => 'Tarjeta de Crédito Cooperativa Guayacán',
         ]);
 
         $tcCope->creditCardDetail()->create([
-            'alias' => 'Cooperativa Guayacán',
+            'alias' => 'Classic',
             'network' => 'Visa',
             'color' => '#6AA82F',
             'last_4' => 3052,
--- database/seeders/InstallmentPlanTableSeeder.php
+++ database/seeders/InstallmentPlanTableSeeder.php
@@ -18,6 +18,36 @@ class InstallmentPlanTableSeeder extends Seeder
         InstallmentPlan::truncate();
 
         InstallmentPlan::create([
+            'name' => 'Apple Watch',
+            'total_amount' => 1799,
+            'total_installments' => 12,
+            'interest_rate' => 0,
+            'start_date' => '2024-12-29',
+            'account_id' => 4,
+            'status' => 'active',
+        ]);
+
+        InstallmentPlan::create([
+            'name' => 'Monito, Teclado y Mouse',
+            'total_amount' => 2467.56,
+            'total_installments' => 12,
+            'interest_rate' => 0,
+            'start_date' => '2025-27-04',
+            'account_id' => 4,
+            'status' => 'active',
+        ]);
+
+        InstallmentPlan::create([
+            'name' => 'Iphone 12 Pro',
+            'total_amount' => 2650,
+            'total_installments' => 12,
+            'interest_rate' => 0,
+            'start_date' => '2025-08-31',
+            'account_id' => 4,
+            'status' => 'active',
+        ]);
+
+        InstallmentPlan::create([
             'name' => 'Closet',
             'total_amount' => 2500,
             'total_installments' => 10,
